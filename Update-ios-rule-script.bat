b6600a65b1a21411d31ea6607e3479b48f9f37ab
hux <hux@fullhan.com>
2023-08-29 09:52:11 +0800

    update List

---
+++ Update-ios-rule-script.bat
@@ -0,0 +1,36 @@
+@echo off
+cd List
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Google/Google.list -O Google.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/YouTube/YouTube.list -O YouTube.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/YouTubeMusic/YouTubeMusic.list -O YouTubeMusic.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GoogleVoice/GoogleVoice.list -O GoogleVoice.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/GitHub/GitHub.list -O GitHub.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/PayPal/PayPal.list -O PayPal.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/TikTok/TikTok.list -O TikTok.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Openproxychains/Openproxychains.list -O Openproxychains.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Facebook/Facebook.list -O Facebook.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Telegram/Telegram.list -O Telegram.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/AppleProxy/AppleProxy.list -O AppleProxy.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Twitter/Twitter.list -O Twitter.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Netflix/Netflix.list -O Netflix.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Tencent/Tencent.list -O Tencent.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/TencentVideo/TencentVideo.list -O TencentVideo.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/WeChat/WeChat.list -O WeChat.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Bing/Bing.list -O Bing.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/360/360.list -O 360.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/NetEase/NetEase.list -O NetEase.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/China/China.list -O China.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/BiliBili/BiliBili.list -O BiliBili.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Bproxychainsdu/Bproxychainsdu.list -O Bproxychainsdu.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Speedtest/Speedtest.list -O Speedtest.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Apple/Apple.list -O Apple.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/iCloud/iCloud.list -O iCloud.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/AppStore/AppStore.list -O AppStore.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/AppleMusic/AppleMusic.list -O AppleMusic.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/AppleID/AppleID.list -O AppleID.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Cloud/CloudCN/CloudCN.list -O CloudCN.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/ChinaMax/ChinaMax.list -O ChinaMax.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/ChinaMax/ChinaMax_Domproxychainsn.list -O ChinaMax_Domproxychainsn.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/ChinproxychainsPs/ChinproxychainsPs.list -O ChinproxychainsPs.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/ChinaASN/ChinaASN.list -O ChinaASN.list
+pause
