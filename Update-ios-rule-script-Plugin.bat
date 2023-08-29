fbe41d6dcf091b7db0a4d2f709d123bc9779e67c
hux <hux@fullhan.com>
2023-08-29 10:15:49 +0800

    update Plugin

---
+++ Update-ios-rule-script-Plugin.bat
@@ -0,0 +1,24 @@
+@echo off
+cd Plugin
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rewrite/Loon/Advertising/Advertising.plugin -O Advertising.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/Amap_remove_ads.plugin -O Amap_remove_ads.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/WexinMiniPrograms_Remove_ads.plugin -O WexinMiniPrograms_Remove_ads.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/Node_detection_tool.plugin -O Node_detection_tool.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/LoonGallery.plugin -O LoonGallery.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/Remove_splash_screen_ads.plugin -O Remove_splash_screen_ads.plugin
+proxychains wget https://raw.githubusercontent.com/app2smile/rules/master/plugin/tieba.plugin -O tieba.plugin
+proxychains wget https://raw.githubusercontent.com/deezertidal/private/master/plugallery/stsq.plugin -O stsq.plugin
+proxychains wget https://raw.githubusercontent.com/deezertidal/private/master/plugallery/colorweather.plugin -O colorweather.plugin
+proxychains wget https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Plugin/BingAI.plugin -O BingAI.plugin
+proxychains wget https://github.com/chavyleung/scripts/raw/master/box/rewrite/boxjs.rewrite.loon.plugin -O boxjs.rewrite.loon.plugin
+proxychains wget https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/config/Loon.plugin -O Loon.plugin
+proxychains wget https://raw.githubusercontent.com/Tartarus2014/Loon-Script/master/Plugin/Block/ZhiHu.plugin -O ZhiHu.plugin
+proxychains wget https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/AutoPolicy/auto-policy.plugin -O auto-policy.plugin
+proxychains wget https://raw.githubusercontent.com/Tartarus2014/Loon-Script/master/Plugin/Block/bilibili.plugin -O bilibili.plugin
+proxychains wget https://raw.githubusercontent.com/Tartarus2014/Loon-Script/master/Plugin/Block/YouTubeAds.plugin -O YouTubeAds.plugin
+proxychains wget https://raw.githubusercontent.com/Semporia/TikTok-Unlock/master/Loon/TikTok-JP.plugin -O TikTok-JP.plugin
+proxychains wget https://raw.githubusercontent.com/Semporia/TikTok-Unlock/master/Loon/TikTok-TW.plugin -O TikTok-TW.plugin
+proxychains wget https://raw.githubusercontent.com/Semporia/TikTok-Unlock/master/Loon/TikTok-KR.plugin -O TikTok-KR.plugin
+proxychains wget https://raw.githubusercontent.com/Semporia/TikTok-Unlock/master/Loon/TikTok-KR.plugin -O TikTok-KR.plugin
+proxychains wget https://raw.githubusercontent.com/Semporia/TikTok-Unlock/master/Loon/TikTok-US.plugin -O TikTok-US.plugin
+pause
