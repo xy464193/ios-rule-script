96a3ffb9f8bbcf1b9fad8852b8cda63252ba9982
hux <hux@fullhan.com>
2023-11-17 08:44:53 +0800

    update UnionPay.list

--- Update-ios-rule-script-List.bat
+++ Update-ios-rule-script-List.bat
@@ -35,4 +35,5 @@ proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/
 proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/ChinaASN/ChinaASN.list -O ChinaASN.list
 proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/Spotify/Spotify.list -O Spotify.list
 proxychains wget https://raw.githubusercontent.com/mphin/ACL4SSR/main/rules/ProxyGFWlist.list -O ProxyGFWlist.list
+proxychains wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/UnionPay/UnionPay.list -O UnionPay.list
 pause
